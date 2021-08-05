b1325416b8b2112fe6e06a1b8b080b4d7a9996cc
Bryce Hoover <bryhoover11@gmail.com>
2021-08-05 12:57:41 -0500

    Added semantic UI, styling, SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-               ReactTube 
+            <div className="ui container">
+               <SearchBar />
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
-const SearchBar = () => {
-    return (
-        <div>
-            SearchBar
-        </div>
-    )
+class SearchBar extends React.Component {
+    state={ term: '' };
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+    
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        // TODO: Call callback from parent component
+    };
+
+    render() {
+        return(
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label htmlFor="search">Video Search</label>
+                        <input 
+                            type="text" 
+                            id="search" 
+                            value={this.state.term} 
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SearchBar
