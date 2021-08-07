bb33f7ca57fe4d912ac0814f4b03a36e2410bb83
Bryce Hoover <bryhoover11@gmail.com>
2021-08-07 14:55:02 -0500

    Event handling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import VideoList from './components/VideoList';
 import Header from './components/Header';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null};
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -17,12 +17,16 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        console.log('From the app', video);
+    }
+
     render() {
         return (
             <div className="ui container">
                <Header />
                <SearchBar onFormSubmit={this.onTermSubmit} />
-               <VideoList videos={this.state.videos} />
+               <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
